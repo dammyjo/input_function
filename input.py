56d98debc856943c9c78f178b2c83e5dbc396e67
dammyjo <112008172+dammyjo@users.noreply.github.com>
2022-08-24 18:46:27 +0100

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+"""
+User Input
+"""
+
+first_name=input(str("Enter your first name:"))
+last_name=input(str("Enter your last name:"))
+age=int (input("Enter your age:"))
+print("Hi",first_name,last_name,"I am", age,"years old")
